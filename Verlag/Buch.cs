b7aed5dcd53860d00bcd33db3b7c60083e3cf84d
scha amin <scha.a2@gso.schule.koeln>
2023-01-13 12:58:12 +0100

    Fortschritt am 13.01.2023

---
+++ Verlag/Buch.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    public class Buch
+    {
+            private string autor;
+            private string titel;
+            private int auflage = 1;
+
+
+            public Buch(string autor, string titel)
+            {
+                this.autor = autor;
+                this.titel = titel;
+            }
+
+            public Buch(string autor, string titel, int auflage) : this(autor, titel)
+            {
+                this.auflage = auflage;
+            
+            }
+
+
+            public string Autor
+            {
+                get
+                {
+                    return autor;
+                }
+                set
+                {
+                    this.autor = value;
+                }
+            }
+
+            public string Titel
+            {
+                get
+                {
+                    return titel;
+                }
+            }
+
+            public int Auflage
+            {
+                get
+                {
+                return this.auflage;
+                }
+                set
+                {
+
+                 this.auflage = value;
+                }
+
+        }
+    }
+}
