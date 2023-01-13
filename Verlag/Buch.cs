96c320fe2af555b9cab184890a6ccae93f4a2833
Salih Azad Salih Salih <salihazad.s@SCHULE.KOELN>
2023-01-13 13:02:13 +0100

    alles außer den letzten Test

---
+++ Verlag/Buch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verlag
+{
+    public class Buch
+    {
+        private string autor;
+        private string titel;
+        private int auflage = 1;
+
+        public Buch(string autor, string titel)
+        { 
+            this.autor = autor;
+            this.titel = titel;
+        }
+
+        public Buch(string autor, string titel, int auflage) : this(autor, titel)
+        {
+            if (auflage > 0)
+            {
+                this.auflage = auflage;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public string Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        public string Titel
+        {
+            get { return titel; }
+        }
+        public int Auflage
+        {
+            get { return auflage; }
+            set
+            {
+                if(value > 0)
+                {
+                    auflage = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
